353457e8ce3db9b9ab1a5d53eac364ff27f163e4
fitboye <49235923+fitboye@users.noreply.github.com>
2024-03-14 16:28:08 -0400

    Minus

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -35,6 +35,13 @@ export default function QueryProcessor(query: string): string {
     return res.toString();
   }
 
+  if (query.startsWith("What is ") && query.includes("multiplied")){
+    let temp = query.match(/\d+/g);
+    if (temp === null) return "";
+    let res = parseInt(temp[0]) - parseInt(temp[1]);
+    return res.toString();
+  }
+
   if (query.startsWith("Which of the following numbers is both a square and a cube: ")){
     let temp = query.match(/\d+/g);
     if (temp === null) return "";
